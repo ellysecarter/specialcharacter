fad9168b7423e05348757bb8aaee654585e362bc
Ellyse <85855848+ellysecarter@users.noreply.github.com>
2022-01-31 22:32:36 -0800

    wrote pseudocode for all function alerts

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,15 +7,46 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   window.alert("Welcome to your personal password generator! Please answer the following prompts for characters you would like to include in your password!")
 
-  window.prompt("How long would you like your password to be? Please choose any number between 8 and 128.")
+  var length = window.prompt("How long would you like your password to be? Please choose any number between 8 and 128.")
+  if (length <= 8 || length >= 128){
+    // add it to the password
 
-  window.alert("Would you like lowercase letters?")
 
-  window.alert("Would you like uppercase letters?")
 
-  window.alert("Would you like numbers?")
 
-  window.alert("Would you like special characters?")
+
+
+
+  } else (window.prompt("Your password needs to be between 8 and 128 characters!"))
+  (writePassword)
+  
+
+  var lowercaseLetters = window.confirm("Would you like lowercase letters?")
+  if (lowercaseLetters){
+      // add it to the password
+
+  }
+
+
+  var uppercaseLetters = window.confirm("Would you like uppercase letters?")
+  if (uppercaseLetters){
+      // add it to the password
+
+  }
+
+
+  var numbers = window.confirm("Would you like numbers?")
+  if (numbers){
+      // add it to the password
+
+  }
+
+
+  var specialCharacters = window.confirm("Would you like special characters?")
+  if (specialCharacters) {
+     // add it to the password
+
+  }
 
 
   var password = generatePassword();
